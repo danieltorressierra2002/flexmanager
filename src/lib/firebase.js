1b46bdebd7816405d72c36f5d4656b483c39b06b
danieltorressierra2002 <danieltorressierra2002@gmail.com>
2026-08-07 15:29:11 -0400

    Add files via upload

---
+++ src/lib/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+// Usa la misma configuración de Firebase que Gym Guerra
+const firebaseConfig = {
+  apiKey: "AIzaSyCd2_9-2CrqWCSACLuM2QK14FTbzYAzbQg",
+  authDomain: "gymguerra-fb628.firebaseapp.com",
+  projectId: "gymguerra-fb628",
+  storageBucket: "gymguerra-fb628.firebasestorage.app",
+  messagingSenderId: "840060700076",
+  appId: "1:840060700076:web:7a9fe613eff110f6ae2609",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export default app;
